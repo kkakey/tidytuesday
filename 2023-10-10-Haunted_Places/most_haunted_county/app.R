161229548bfd240ac3c3efc8940de3b77dab0cf7
kkakey <kkakey999@gmail.com>
2023-11-03 09:35:38 -0400

    Add icons/fonts

--- 2023-10-10-Haunted_Places/most_haunted_county/app.R
+++ 2023-10-10-Haunted_Places/most_haunted_county/app.R
@@ -1,67 +1,105 @@
 library(shiny)
 library(leaflet)
 library(sf)
+library(tidyr)
 
 # Data ------------------------------------------------------------------
 
-haunted_places <- read_sf("../output_data/haunted_places_county.shp")
+haunted_places <- read_sf("../output_data/haunted_places_county-final2.shp")
+haunted_places$lng <- purrr::map_vec(haunted_places$geometry, ~st_coordinates(.))[,1] %>% as.character()
+haunted_places$lat <- purrr::map_vec(haunted_places$geometry, ~st_coordinates(.))[,2] %>% as.character()
 state_filter <-read_sf("../output_data/state_filter.shp")
+firstup <- function(x) {
+  substr(x, 1, 1) <- toupper(substr(x, 1, 1))
+  x
+}
 
 ## ghost icon https://icons8.com/icons/set/ghost
 ghostIcons <- icons(
-  iconUrl = '/Users/kristenakey/Downloads/icons8-ghost-48-color.png',
-  iconWidth = 23, iconHeight = 23#,
-  # shadowUrl = '/Users/kristenakey/Downloads/output-onlinepngtools.png',
-  # shadowWidth = 26, shadowHeight = 26,
-  # shadowAnchorX = .01, shadowAnchorY = .01
+  iconUrl = './icon/icons8-ghost-48.png',
+  iconWidth = 23, iconHeight = 23
 )
 
 # Dashboard ----------------------------------------------------------------
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
+  includeCSS("www/style.css"),
+  h1("Haunted Places in Los Angeles County"),
+  h2(HTML('LA County is the most haunted county in the United States, with over 300 reported haunted places. Click the ghosts to learn more about each haunted place.')),
+  # h3("LA County is the most haunted county in the United States, with over 300 reported haunted places. Click the ghosts to learn more about each haunted place."),
   fluidRow(
     column(4,
-           h1("Header here"),
-           h3("more text"),
-           plotOutput("imageOutput")
+           br(), br(),
+           h3(textOutput("location")),
+           h4(textOutput("city")),
+           br(),
+           textOutput("description"),
+           uiOutput("image")
     ),
     column(8,
-           leafletOutput("mymap", height=700)
+           leafletOutput("map", height=700),
+           actionButton("reset_button", "Reset view", class="button"),
+           br(),
+           p("Data:"),
+           p("amde by")
     )
   )
-  # actionButton("recalc", "New points")
 
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output, session) {
 
-  # points <- eventReactive(input$recalc, {
-  #   cbind(rnorm(40) * 2 + 13, rnorm(40) + 48)
-  # }, ignoreNULL = FALSE)
-
-  output$mymap <- renderLeaflet({
+  output$map <- renderLeaflet({
     leaflet() %>%
       fitBounds(-118.9, 33.68, -117.7, 34.8) %>%
-      # setView(lat = 34.2, lng = -118.11, zoom = 9) %>%
+      setView(lat = 34.2, lng = -118.11, zoom = 9) %>%
       addProviderTiles(providers$Stadia.AlidadeSmoothDark,
                        options = providerTileOptions(noWrap = TRUE)
       ) %>%
       addPolygons(data = state_filter, stroke = FALSE, fillOpacity = 0.5, smoothFactor = 0.5,
                   color = "black", opacity = 1) %>%
       addMarkers(data = haunted_places, icon = ghostIcons,
-      )
+                  popup = ~locatin)
+  })
+
+  observe({input$reset_button
+    leafletProxy("map") %>% setView(lat = 34.2, lng = -118.11, zoom = 9)
+  })
+
+  observeEvent(input$map_marker_click, {
+    click <- input$map_marker_click
+    loc <- haunted_places[which(haunted_places$lat == as.character(click$lat) & haunted_places$lng == as.character(click$lng)), ]$locatin[1]
+    city <- haunted_places[which(haunted_places$lat == as.character(click$lat) & haunted_places$lng == as.character(click$lng)), ]$city[1]
+    desc <- haunted_places[which(haunted_places$lat == as.character(click$lat) & haunted_places$lng == as.character(click$lng)), ]$dscrptn[1]
+    img_url_click <- haunted_places[which(haunted_places$lat == as.character(click$lat) & haunted_places$lng == as.character(click$lng)), ]$img_url[1]
+    # https://www.pinterest.com/pin/characters--579979258278605737/
+    img_url_click <- ifelse(is.na(img_url_click), 'Halloween Play Sticker by Andrew Onorato for iOS & Android _ GIPHY.gif', img_url_click)
+
+    output$location <- renderText({
+      stringr::str_to_title(loc)
+    })
+
+    output$city <- renderText({
+      paste(" in ", stringr::str_to_title(city))
+    })
+
+    output$description <- renderText({
+      firstup(desc)
+    })
+
+    output$image <- renderUI({
+      tags$img(src = img_url_click, height="100%", width="100%", align="left")
+    })
+
   })
 }
 
-# Run the application
 shinyApp(ui = ui, server = server)
 
 
-## to do:
-### add auto zoom in on county
-### on-hover: place info show ups
-### on-click: photo shows up
-## ghost shadows?
+### CSS/make pretty!
+### fix descriptions -- long ones get cut off
+### Add data source
+### Fix some images not matching
+###
